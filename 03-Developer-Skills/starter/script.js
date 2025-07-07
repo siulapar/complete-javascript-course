6a726c1f911dfa0cbffc7049c9d07eddeed8cd16
Jeff Wong Yih Suei <jeffwong@cloudflare.com>
2025-07-07 21:01:16 +0800

    Nodejs live-server.

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,10 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const x = 23;
+
+// TODO:
+
+const calAge = (birthYear) => 2025 - birthYear;
+
+console.log(calAge(1972));
