e38fa63f634a0bba931f429e13641addf1024460
Jeff Wong Yih Suei <jeffwong@cloudflare.com>
2025-08-09 01:00:45 +0800

    Try coding the example.

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -59,23 +59,34 @@ const calWorkHours = function (arr) {
   let sum = 0;
   let maxHr = 0;
   let maxDay = 0;
+  let numDayWork = 0;
   let i = 1;
   for (const hr of arr) {
     sum += hr;
+
+    if (hr > 0) numDayWork += 1;
+
     if (hr > maxHr) {
       maxHr = hr;
       maxDay = i;
     }
+
     i += 1;
   }
+
+  let fullTime = false;
+  if (sum >= 35.0) fullTime = true;
 
   console.log("Total hours worked =", sum);
 
   const avgDailyHours = sum / arr.length;
   console.log("Average daily hours =", avgDailyHours);
   console.log("The day with the most hours worked =", maxHr, "Day =", maxDay);
+  console.log(`Number of days worked =`, numDayWork);
+  console.log("Week was full time(35 hours or more)?", fullTime);
 };
 
 const testData = [7.5, 8, 6.5, 0, 8.5, 4, 0];
 
+console.log(testData);
 calWorkHours(testData);
