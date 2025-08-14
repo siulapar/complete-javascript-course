8a9c35abe4105f482153c519306f2f83ea2c5795
Jeff Wong Yih Suei <jeffwong@cloudflare.com>
2025-08-14 12:19:14 +0800

    Guess number app

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,35 +1,49 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = '🎉🎉🎉 Correct Number!';
+document.querySelector('.score').textContent = 0;
 
-// document.querySelector('.number').textContent = 17;
-// document.querySelector('.score').textContent = 88;
+let number = Math.round(Math.random() * 20);
+// console.log(number);
 
-// document.querySelector('.guess').value = 28;
-// console.log(document.querySelector('.guess').value);
-
-const number = Math.round(Math.random() * 20);
-
-console.log(number);
+let i = 1;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
-
-  if (guess === number) {
-    document.querySelector('.message').textContent = '🎉🎉🎉 Correct Number!';
-  } else if (guess > number) {
-    document.querySelector('.message').textContent = '🐘 Too big!';
-  } else {
-    document.querySelector('.message').textContent = '🐭 Too small!';
-  }
+  // console.log(guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '⚠️ No number!';
+  } else {
+    if (guess === number) {
+      document.querySelector('.message').textContent = '🎉🎉🎉 Correct Number!';
+      document.querySelector('.number').textContent = number;
+      document.querySelector('.score').textContent = i;
+      highScore = Number(document.querySelector('.highscore').textContent);
+      if (!highScore) {
+        document.querySelector('.highscore').textContent = i;
+      } else {
+        if (i < highScore) {
+          document.querySelector('.highscore').textContent = i;
+        }
+      }
+    } else if (guess > number) {
+      document.querySelector('.message').textContent = '🐘 Too big!';
+      i += 1;
+    } else {
+      document.querySelector('.message').textContent = '🐭 Too small!';
+      i += 1;
+    }
   }
+  console.log(i);
 });
 
 document.querySelector('.again').addEventListener('click', function () {
-  location.reload();
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = 0;
+  i = 1;
+  number = Math.round(Math.random() * 20);
+  // console.log(number);
 });
