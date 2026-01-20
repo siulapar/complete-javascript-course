fd48e2abd44eea767b934bddb11514a89e280e91
jeffwong <wongyihsuei@gmail.com>
2026-01-20 18:15:57 +0800

    Learn call, apply and bind.

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// function greet(greeting) {
+//     function innerGreet(name) {
+//         return `${greeting}, ${name}!`;
+//     }
+//   return innerGreet;
+// }
+
+const greet = (greeting) => (name) => `${greeting}, ${name}!`;
+
+
+console.log(greet('Hello')('Alice')); // Output: Hello, Alice!
+
+const sayHi = greet('Hi');
+console.log(sayHi('Bob')); // Output: Hi, Bob!
+console.log(sayHi('Charlie')); // Output: Hi, Charlie!
